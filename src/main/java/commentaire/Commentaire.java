18794eae7f000d8ae3eff94a08ab70ac66f64c01
Mamy <rasolofomananaaiglon@gmail.com>
2017-02-13 14:23:06 +0300

    conect

--- src/main/java/commentaire/Commentaire.java
+++ src/main/java/commentaire/Commentaire.java
@@ -3,23 +3,32 @@ import defi.Defi;
 
 public class Commentaire extends Defi
 {
-	private String idComm;
-	private String contenu;
+    private String user;
+    private String idComm;
+    private String contenu;
 	
-	public void setIdComm(String idComm)
-	{
-		this.idComm=idComm;
-	}
-	public void setContenu(String contenu)
-	{
-		this.contenu=contenu;
-	}
-	public String getIdComm()
-	{
-		return idComm;
-	}
-	public String getContenu()
-	{
-		return contenu;
-	}
+    public void setIdComm(String idComm)
+    {
+            this.idComm=idComm;
+    }
+    public void setContenu(String contenu)
+    {
+            this.contenu=contenu;
+    }
+    public String getIdComm()
+    {
+            return idComm;
+    }
+    public String getContenu()
+    {
+            return contenu;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
 }
